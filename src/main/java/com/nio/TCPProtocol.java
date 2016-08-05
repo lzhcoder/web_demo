895ae444a534e54a9bbae680547f211f4d57fb19
623196924@qq.com <623196924@qq.com>
2016-08-05 13:57:19 +0800

    nio

---
+++ src/main/java/com/nio/TCPProtocol.java
@@ -0,0 +1,11 @@
+package com.nio;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+public interface TCPProtocol {
+	
+	void handleAccept(SelectionKey key) throws IOException;
+	void handleRead(SelectionKey key) throws IOException;
+	void handleWrite(SelectionKey key) throws IOException;
+}
