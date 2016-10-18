3f18baa01243d9ba8fd01717583e127ee088bbc6
623196924@qq.com <623196924@qq.com>
2016-10-18 10:44:36 +0800

    增加redis测试

--- src/main/java/com/redis/RedisClient.java
+++ src/main/java/com/redis/RedisClient.java
@@ -360,71 +360,79 @@ public class RedisClient {
 	}
 
 	private void SetOperate() {
-
+		 System.out.println("======================set=========================="); 
+	        // 清空数据 
+	        System.out.println("清空库中所有数据："+jedis.flushDB());
+	        
+	        System.out.println("=============增=============");
+	        System.out.println("向sets集合中加入元素element001："+jedis.sadd("sets", "element001")); 
+	        System.out.println("向sets集合中加入元素element002："+jedis.sadd("sets", "element002")); 
+	        System.out.println("向sets集合中加入元素element003："+jedis.sadd("sets", "element003"));
+	        System.out.println("向sets集合中加入元素element004："+jedis.sadd("sets", "element004"));
+	        System.out.println("查看sets集合中的所有元素:"+jedis.smembers("sets")); 
+	        System.out.println();
+	        
+	        System.out.println("=============删=============");
+	        System.out.println("集合sets中删除元素element003："+jedis.srem("sets", "element003"));
+	        System.out.println("查看sets集合中的所有元素:"+jedis.smembers("sets"));
+	        /*System.out.println("sets集合中任意位置的元素出栈："+jedis.spop("sets"));//注：出栈元素位置居然不定？--无实际意义
+	        System.out.println("查看sets集合中的所有元素:"+jedis.smembers("sets"));*/
+	        System.out.println();
+	        
+	        System.out.println("=============改=============");
+	        System.out.println();
+	        
+	        System.out.println("=============查=============");
+	        System.out.println("判断element001是否在集合sets中："+jedis.sismember("sets", "element001"));
+	        System.out.println("循环查询获取sets中的每个元素：");
+	        Set<String> set = jedis.smembers("sets");   
+	        Iterator<String> it=set.iterator() ;   
+	        while(it.hasNext()){   
+	            Object obj=it.next();   
+	            System.out.println(obj);   
+	        }  
+	        System.out.println();
+	        
+	        System.out.println("=============集合运算=============");
+	        System.out.println("sets1中添加元素element001："+jedis.sadd("sets1", "element001")); 
+	        System.out.println("sets1中添加元素element002："+jedis.sadd("sets1", "element002")); 
+	        System.out.println("sets1中添加元素element003："+jedis.sadd("sets1", "element003")); 
+	        System.out.println("sets1中添加元素element002："+jedis.sadd("sets2", "element002")); 
+	        System.out.println("sets1中添加元素element003："+jedis.sadd("sets2", "element003")); 
+	        System.out.println("sets1中添加元素element004："+jedis.sadd("sets2", "element004"));
+	        System.out.println("查看sets1集合中的所有元素:"+jedis.smembers("sets1"));
+	        System.out.println("查看sets2集合中的所有元素:"+jedis.smembers("sets2"));
+	        System.out.println("sets1和sets2交集："+jedis.sinter("sets1", "sets2"));
+	        System.out.println("sets1和sets2并集："+jedis.sunion("sets1", "sets2"));
+	        System.out.println("sets1和sets2差集："+jedis.sdiff("sets1", "sets2"));//差集：set1中有，set2中没有的元素
 	}
 
 	private void SortedSetOperate() {
-		System.out
-				.println("======================set==========================");
-		// 清空数据
-		System.out.println("清空库中所有数据：" + jedis.flushDB());
-
-		System.out.println("=============增=============");
-		System.out.println("向sets集合中加入元素element001："
-				+ jedis.sadd("sets", "element001"));
-		System.out.println("向sets集合中加入元素element002："
-				+ jedis.sadd("sets", "element002"));
-		System.out.println("向sets集合中加入元素element003："
-				+ jedis.sadd("sets", "element003"));
-		System.out.println("向sets集合中加入元素element004："
-				+ jedis.sadd("sets", "element004"));
-		System.out.println("查看sets集合中的所有元素:" + jedis.smembers("sets"));
-		System.out.println();
-
-		System.out.println("=============删=============");
-		System.out.println("集合sets中删除元素element003："
-				+ jedis.srem("sets", "element003"));
-		System.out.println("查看sets集合中的所有元素:" + jedis.smembers("sets"));
-		/*
-		 * System.out.println("sets集合中任意位置的元素出栈："+jedis.spop("sets"));//注：
-		 * 出栈元素位置居然不定？--无实际意义
-		 * System.out.println("查看sets集合中的所有元素:"+jedis.smembers("sets"));
-		 */
-		System.out.println();
-
-		System.out.println("=============改=============");
-		System.out.println();
-
-		System.out.println("=============查=============");
-		System.out.println("判断element001是否在集合sets中："
-				+ jedis.sismember("sets", "element001"));
-		System.out.println("循环查询获取sets中的每个元素：");
-		Set<String> set = jedis.smembers("sets");
-		Iterator<String> it = set.iterator();
-		while (it.hasNext()) {
-			Object obj = it.next();
-			System.out.println(obj);
-		}
-		System.out.println();
-
-		System.out.println("=============集合运算=============");
-		System.out.println("sets1中添加元素element001："
-				+ jedis.sadd("sets1", "element001"));
-		System.out.println("sets1中添加元素element002："
-				+ jedis.sadd("sets1", "element002"));
-		System.out.println("sets1中添加元素element003："
-				+ jedis.sadd("sets1", "element003"));
-		System.out.println("sets1中添加元素element002："
-				+ jedis.sadd("sets2", "element002"));
-		System.out.println("sets1中添加元素element003："
-				+ jedis.sadd("sets2", "element003"));
-		System.out.println("sets1中添加元素element004："
-				+ jedis.sadd("sets2", "element004"));
-		System.out.println("查看sets1集合中的所有元素:" + jedis.smembers("sets1"));
-		System.out.println("查看sets2集合中的所有元素:" + jedis.smembers("sets2"));
-		System.out.println("sets1和sets2交集：" + jedis.sinter("sets1", "sets2"));
-		System.out.println("sets1和sets2并集：" + jedis.sunion("sets1", "sets2"));
-		System.out.println("sets1和sets2差集：" + jedis.sdiff("sets1", "sets2"));// 差集：set1中有，set2中没有的元素
+		  System.out.println("======================zset=========================="); 
+	        // 清空数据 
+	        System.out.println(jedis.flushDB()); 
+	        
+	        System.out.println("=============增=============");
+	        System.out.println("zset中添加元素element001："+shardedJedis.zadd("zset", 7.0, "element001")); 
+	        System.out.println("zset中添加元素element002："+shardedJedis.zadd("zset", 8.0, "element002")); 
+	        System.out.println("zset中添加元素element003："+shardedJedis.zadd("zset", 2.0, "element003")); 
+	        System.out.println("zset中添加元素element004："+shardedJedis.zadd("zset", 3.0, "element004"));
+	        System.out.println("zset集合中的所有元素："+shardedJedis.zrange("zset", 0, -1));//按照权重值排序
+	        System.out.println();
+	        
+	        System.out.println("=============删=============");
+	        System.out.println("zset中删除元素element002："+shardedJedis.zrem("zset", "element002"));
+	        System.out.println("zset集合中的所有元素："+shardedJedis.zrange("zset", 0, -1));
+	        System.out.println();
+	        
+	        System.out.println("=============改=============");
+	        System.out.println();
+	        
+	        System.out.println("=============查=============");
+	        System.out.println("统计zset集合中的元素中个数："+shardedJedis.zcard("zset"));
+	        System.out.println("统计zset集合中权重某个范围内（1.0——5.0），元素的个数："+shardedJedis.zcount("zset", 1.0, 5.0));
+	        System.out.println("查看zset集合中element004的权重："+shardedJedis.zscore("zset", "element004"));
+	        System.out.println("查看下标1到2范围内的元素值："+shardedJedis.zrange("zset", 1, 2));
 
 	}
 
